12196e3e172557176064a8610fa5af2ec44a549f
RajarshiRay25 <rayrajarshi9@gmail.com>
2022-01-27 21:21:50 +0530

    USER 1-D

---
+++ 1-D_User.py
@@ -0,0 +1,20 @@
+import numpy as np
+matrix =[]
+
+row=int(input("Enter Rows ->"))
+col=int(input("Enter Columns ->"))
+
+for i in range (row):
+    a=[]
+    for j in range(col):
+        col_val = int(input(f"Enter Values for {i} Row & {j} Col "))
+        a.append(col_val)
+
+    matrix.append(a)
+arr = np.array(matrix
+)
+for i in range(row):
+    for j in range (col):
+        print(arr[i][j] , end=" ")
+    
+    print()
